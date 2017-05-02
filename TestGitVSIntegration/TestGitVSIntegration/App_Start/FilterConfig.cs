00643c3ec7fa049527cfe064d25e0c2ee79df1cd
Jeremiah Turco <turco.jeremiah@gmail.com>
2017-05-02 08:46:18 -0600

    This is a test commit into github from visual studio

---
+++ TestGitVSIntegration/TestGitVSIntegration/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TestGitVSIntegration
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TestGitVSIntegration/TestGitVSIntegration/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestGitVSIntegration.MvcApplication" Language="C#" %>
